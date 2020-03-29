bfdebd9ac92497f2841a5dc4fecd38f494c57b56
Alex <anmunoz24@gmail.com>
2020-03-29 17:49:41 -0500

    ui form

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 
 
 const SignUp = () => {
+
+const [artistType, setArtistType] = React.useState('');
+
+const artistChange = event => {
+setArtistType(event.target.value);
+// console.log(event.target.value)
+};
+
+const [genreType, setGenreType] = React.useState('');
+
+const genreChange = event => {
+  setGenreType(event.target.value);
+}
+
+  
 return (<>
 
     <div class="sign">
@@ -11,32 +30,44 @@ return (<>
         </div>
 
     <div className="signup-container">
-        <h2 className="signup-header">Invest In Yourself</h2>
+        <h2 className="signup-header">MicUS Sign Up</h2>
 
         <Form className="signup-form">
         <Form.Group>
-            <Form.Label>Artist Name</Form.Label>
-        <Form.Control placeholder="Artist Name" />
+        <Form.Control className="style" placeholder="Artist Name" />
+        <Form.Control className="style" placeholder="Email Address" />
 
-       
-      <Form.Label>Type Of Artist</Form.Label>
-      <Form.Control as="select" value="Choose...">
-        <option>Band</option>
-        <option>Solo</option>
-        <option>DJ</option>
-      </Form.Control>
- 
-      <Form.Label>Genre Type</Form.Label>
-      <Form.Control as="select" value="Choose...">
-      <option>Alternative/Rock</option>
-        <option>Country/Folk</option>
-        <option>Hip Hop/Rap</option>
-        <option>Top 40/Pop</option>
-        <option>Classical/Jazz</option>
-      </Form.Control>
-       
-      <Form.Label>Email Address</Form.Label>
-      <Form.Control placeholder="Email" />
+
+       <div className="wrapper">
+       <TextField
+          id="outlined-select-currency"
+          select
+          label="Type Of Artist"
+          value={artistType}
+          onChange={artistChange}
+          variant="outlined"
+        >
+          <MenuItem value={1}>Band</MenuItem>
+          <MenuItem value={2}>Solo</MenuItem>
+          <MenuItem value={3}>DJ</MenuItem>
+        </TextField>
+
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Genre Type"
+          value={genreType}
+          onChange={genreChange}
+          helperText="Select an option"
+          variant="outlined"
+        >
+          <MenuItem value={1}>Rock/Alternative</MenuItem>
+          <MenuItem value={2}>Rap/Hip Hop</MenuItem>
+          <MenuItem value={3}>Country/Folk</MenuItem>
+          <MenuItem value={4}>Jazz/Classical</MenuItem>
+        </TextField>
+      </div>
+
       </Form.Group>
       </Form> 
 
