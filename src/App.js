80d5e2f4abfbe252e51c951977e491070a4f55a0
Alex <anmunoz24@gmail.com>
2020-03-30 19:37:24 -0500

    implement redux

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Navbar from './Components/Navbar'
 import Router from '../src/Router'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import '/Users/luismunoz/Documents/Mic-Us-App/mic-us/src/App.css';
 
 class App extends Component {
   render () {
     return (
+      <Provider store={store}>
       <BrowserRouter>
         <Navbar/>
         <Router />
       </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
@@ -23,63 +21,59 @@ const genreChange = event => {
 }
 
   
-return (<>
-
-    <div class="sign">
-          <div class="signup__background-image"></div>
-        </div>
+return (
+<>
+  <div class="sign">
+    <div class="signup__background-image"></div>
+  </div>
 
     <div className="signup-container">
         <h2 className="signup-header">MicUS Sign Up</h2>
 
-        <Form className="signup-form">
-        <Form.Group>
+    <Form className="signup-form">
+      <Form.Group>
         <Form.Control className="style" placeholder="Artist Name" />
+
         <Form.Control className="style" placeholder="Email Address" />
 
+        <div className="wrapper">
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Type Of Artist"
+            value={artistType}
+            onChange={artistChange}
+            variant="outlined">
+              <MenuItem value={1}>Band</MenuItem>
+              <MenuItem value={2}>Solo</MenuItem>
+              <MenuItem value={3}>DJ</MenuItem>
+          </TextField>
 
-       <div className="wrapper">
-       <TextField
-          id="outlined-select-currency"
-          select
-          label="Type Of Artist"
-          value={artistType}
-          onChange={artistChange}
-          variant="outlined"
-        >
-          <MenuItem value={1}>Band</MenuItem>
-          <MenuItem value={2}>Solo</MenuItem>
-          <MenuItem value={3}>DJ</MenuItem>
-        </TextField>
-
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Genre Type"
-          value={genreType}
-          onChange={genreChange}
-          helperText="Select an option"
-          variant="outlined"
-        >
-          <MenuItem value={1}>Rock/Alternative</MenuItem>
-          <MenuItem value={2}>Rap/Hip Hop</MenuItem>
-          <MenuItem value={3}>Country/Folk</MenuItem>
-          <MenuItem value={4}>Jazz/Classical</MenuItem>
-        </TextField>
-      </div>
-
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Genre Type"
+            value={genreType}
+            onChange={genreChange}
+            variant="outlined">
+              <MenuItem value={1}>Rock/Alternative</MenuItem>
+              <MenuItem value={2}>Rap/Hip Hop</MenuItem>
+              <MenuItem value={3}>Country/Folk</MenuItem>
+              <MenuItem value={4}>Jazz/Classical</MenuItem>
+          </TextField>
+        </div>
       </Form.Group>
-      </Form> 
+    </Form> 
 
-      <Link to="">
-          <Button className="register-button">
-              <span>Register Now!</span>
-          </Button>     
-      </Link> 
+    <Link to="">
+      <Button className="register-button">
+        <span>Register Now!</span>
+      </Button>     
+    </Link> 
       
-    </div>
-       </>
-    )
+  </div>
+</>
+  )
 }
 
 export default SignUp;
